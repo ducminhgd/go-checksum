791f07106012ff90aa68d2cd8086dfff271d2417
Giã Dương Đức Minh <minh.gia@vieon.vn>
2022-03-08 10:22:50 +0700

    Error: reading inputFile

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/hex"
+	"io"
 	"log"
 	"os"
 
@@ -12,6 +13,8 @@ import (
 
 var (
 	command, inputFile, checksum string
+	CMD_CREATE                   string = "create"
+	CMD_VERIFY                   string = "verify"
 )
 
 var cmdTemplate = &cobra.Command{
@@ -22,17 +25,44 @@ var cmdTemplate = &cobra.Command{
 		if err != nil {
 			log.Println("Command is invalid")
 		}
+		if command == "" {
+			command = args[0]
+		}
 		inputFile, _ := cmd.Flags().GetString("input-file")
 		checksum, _ := cmd.Flags().GetString("checksum")
+
+		cmdList := []string{CMD_CREATE, CMD_VERIFY}
+		isInList := false
+		for _, c := range cmdList {
+			if c == command {
+				isInList = true
+				break
+			}
+		}
+		if !isInList {
+			log.Fatalf("Invalid command. Support: %s\n", cmdList)
+		}
+
+		if inputFile == "" {
+			log.Fatalln("Missing file path")
+		}
+		log.Println(inputFile)
+
+		if command == CMD_VERIFY && checksum == "" {
+			log.Fatalln("Missing checksum string")
+		}
 	},
 }
 
 func main() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
 	cmdTemplate.Flags().StringP("command", "c", "", "Command: create or verify")
 	cmdTemplate.Flags().StringP("input-file", "i", "", "Input file path")
 	cmdTemplate.Flags().StringP("sum-string", "s", "", "Checksum string to verify with input file")
 	cmdTemplate.Execute()
 
+	log.Println(inputFile)
 	file, err := os.Open(inputFile)
 
 	if err != nil {
@@ -41,11 +71,9 @@ func main() {
 	}
 	defer file.Close()
 
-	dataBuffer := make([]byte, 1024*10)
 	digest := md5.New()
-	for {
-		digest.Write(dataBuffer)
+	if _, err := io.Copy(digest, file); err != nil {
+		log.Fatalln(err)
 	}
-	r := digest.Sum(nil)
-	return hex.EncodeToString(r)
+	log.Println(hex.EncodeToString(digest.Sum(nil)))
 }
